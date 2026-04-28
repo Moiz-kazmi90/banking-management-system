3e4ee58f89c4e32e603bae576b4f7a9ea1cd04f0
moiz <syedmuhammadmoizkazmi151@gmail.com>
2026-04-28 14:58:07 +0500

    add vercel URL to CORS

--- backened/src/app.js
+++ backened/src/app.js
@@ -12,7 +12,8 @@ const app = express()
 
 // middlewares
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [ "http://localhost:5173",
+   "https://banking-management-system-one.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"]
